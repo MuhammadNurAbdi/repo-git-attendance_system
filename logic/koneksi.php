246b2f8712782ab3aa6687165024142c293f2896
zeventier <zeventier@gmail.com>
2021-12-01 12:25:28 +0800

    mengembalikan koneksi ke remote database

--- logic/koneksi.php
+++ logic/koneksi.php
@@ -2,14 +2,14 @@
 try {
     $pdo_conn = new PDO(
         //Development Connection
-        'mysql:host=localhost;dbname=attendancedb',
-        'root',
-        '',
+        // 'mysql:host=localhost;dbname=attendancedb',
+        // 'root',
+        // '',
 
         //Remote Database Connection
-        // 'mysql:host=remotemysql.com;dbname=dGQZe91J3b',
-        // 'dGQZe91J3b',
-        // 'kO6GSHkFS8',
+        'mysql:host=remotemysql.com;dbname=dGQZe91J3b',
+        'dGQZe91J3b',
+        'kO6GSHkFS8',
         array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true)
     );
 } catch (PDOException $e) {
